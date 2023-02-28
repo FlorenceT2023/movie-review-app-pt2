b8db600def73b7e7d2454096f85b4e94f0935dcf
Florence Tat <florence.tat28@ed.cna.nl.ca>
2023-02-28 09:56:41 -0330

    new branch

--- movie-review-app-backend/src/server.js
+++ movie-review-app-backend/src/server.js
@@ -1,18 +1,23 @@
 import express from 'express';
+import path from 'path';
 import fs from 'fs';
-import {MongoClient} from 'mongodb';
+import { MongoClient } from 'mongodb';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const app = express();
 const port = 8000;
 
-
 app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, '../build')));
 
 app.get('/', (req, res) => {
     res.send('Hello');
 })
 
-// const movieData = JSON.parse(fs.readFileSync('./movies.json'));
-// console.log(movieData);
+
 
 app.get('/movies', async (req, res) => {
     const client = new MongoClient('mongodb://127.0.0.1:27017');
@@ -31,28 +36,12 @@ app.post('/review', async (req, res) => {
     await client.connect();
 
     const db = client.db('movie-data');
-    // is there a way to generate movie id from here?
     
     const insertOperation = await db.collection('movies').insertOne( {"title":req.body.title, "release_date":req.body.release_date, "actors": req.body.actors, "image": req.body.image, "rating": parseInt(req.body.rating)} );
     console.log(insertOperation);
     res.redirect('/');
 
-    // movieData.push( {"id": movieData.length+1, "title":req.body.title, "release_date":req.body.release_date, "actors": req.body.actors, "image": req.body.image, "rating": req.body.rating} )
-    // saveData();
-    // console.log('update movies called')
-    // console.log(req.body);
-    // res.redirect('/');
 })
-
-const saveData = () => {
-    const jsonContent = JSON.stringify(movieData);
-    fs.writeFile("./movies.json", jsonContent, 'utf8', function (err) {
-        if (err) {
-            console.log("An error occurred while writing JSON Object to file.");
-        }
-        console.log("JSON file has been saved.");
-    });
-}
 
 app.listen(8000, () => {
     console.log(`Listening on port ${port}`);
